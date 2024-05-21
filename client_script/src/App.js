a3e3e0d187fce882b8a4fd36978f1871d5bcd529
samdaniel-1 <81462039+samdaniel-1@users.noreply.github.com>
2024-05-21 19:15:48 +0530

    SRC completed

---
+++ client_script/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Branch from './Tree/branch';
+// import View from './components/view';
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+function App() {
+  return (
+    <>
+    <Branch/>
+    
+    </>
+  );
+}
+
+export default App;
